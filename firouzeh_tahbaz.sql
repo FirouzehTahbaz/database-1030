a978f4d27d02c806ea974a8becd83ef5a1fd8225
FirouzehTahbaz <84801753+FirouzehTahbaz@users.noreply.github.com>
2021-08-22 20:53:45 -0400

    Update firouzeh_tahbaz.sql

--- firouzeh_tahbaz.sql
+++ firouzeh_tahbaz.sql
@@ -1,22 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Aug 13, 2021 at 11:49 PM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `firouzeh_tahbaz`
